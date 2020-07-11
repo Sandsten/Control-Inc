db2025c25f6b6913dd6929c673be449aadf11a2f
Anton Lövström <anton.lovstrom4@gmail.com>
2020-07-11 13:14:22 +0200

    Basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb  = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+    }
+}
