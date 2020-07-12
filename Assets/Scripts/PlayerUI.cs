64a062761f4c0062da1994156d8c7f791e05cdda
Sandsten <staffansandberg@outlook.com>
2020-07-12 11:29:05 +0200

    Mana bars functionality

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -8,6 +8,19 @@ public class PlayerUI : MonoBehaviour
 {
     public static PlayerUI instance;
     private Text text;
+    public Player player;
+
+    [Header("Mana bars")]
+    public Canvas manaBars;
+    public Image manaUpBar;
+    public Image manaDownBar;
+    public Image manaRightBar;
+    public Image manaLeftBar;
+
+    public Color fullManaColor;
+    public Color emptyManaColor;
+
+    private Color defaultManaBarColor;
 
     private void Awake()
     {
@@ -51,6 +64,16 @@ public class PlayerUI : MonoBehaviour
         rectTransform = text.GetComponent<RectTransform>();
         rectTransform.localPosition = new Vector3(-150, 90, 0);
         rectTransform.sizeDelta = new Vector2(600, 200);
+
+        // int numberOfBars = manaBars.transform.childCount;
+        DontDestroyOnLoad(manaBars);
+        // for(int i = 0; i < numberOfBars; i++){
+        //     GameObject o = manaBars.transform.GetChild(i).gameObject;
+        //     o.transform.parent = canvas.transform;
+        //     DontDestroyOnLoad(o);
+        // }
+        defaultManaBarColor = manaLeftBar.color;
+
     }
 
     // Start is called before the first frame update
@@ -68,7 +91,45 @@ public class PlayerUI : MonoBehaviour
         " Mana backward: " + Math.Round(GameObject.Find("Player").GetComponent<Player>().manaBackwards).ToString() +
         " Mana right: " + Math.Round(GameObject.Find("Player").GetComponent<Player>().manaRight).ToString() +
         " Mana left: " + Math.Round(GameObject.Find("Player").GetComponent<Player>().manaLeft).ToString();
+
+        UpdateManaBars();
     }
+
+
+    void UpdateManaBars() 
+    {   
+        // ManaLeft
+        float barWidthLeft = GetManaBarWidth(player.manaLeft);
+        manaLeftBar.rectTransform.sizeDelta =  new Vector2(barWidthLeft, manaLeftBar.rectTransform.sizeDelta.y);
+        manaLeftBar.color = GetManaBarColor(player.manaLeft);
+
+        float barWidthRight = GetManaBarWidth(player.manaRight);
+        manaRightBar.rectTransform.sizeDelta =  new Vector2(barWidthRight, manaRightBar.rectTransform.sizeDelta.y);
+        manaRightBar.color = GetManaBarColor(player.manaRight);
+
+        float barWidthUp = GetManaBarWidth(player.manaForward);
+        manaUpBar.rectTransform.sizeDelta =  new Vector2(barWidthUp, manaUpBar.rectTransform.sizeDelta.y);
+        manaUpBar.color = GetManaBarColor(player.manaForward);
+
+        float barWidthDown = GetManaBarWidth(player.manaBackwards);
+        manaDownBar.rectTransform.sizeDelta =  new Vector2(barWidthDown, manaDownBar.rectTransform.sizeDelta.y);
+        manaDownBar.color = GetManaBarColor(player.manaBackwards);
+    }
+
+    float GetManaBarWidth(float remainingMana) 
+    {
+        float percentMana = remainingMana/player.maxMana;
+        return Mathf.Lerp(10, 230, percentMana);
+    }
+
+    Color GetManaBarColor(float remainingMana)
+    {
+        float percentMana = remainingMana/player.maxMana;
+        Color c = Color.Lerp(emptyManaColor, fullManaColor, percentMana);
+        c.a = 1.0f;
+        return c;
+    }
+
 
     /* public void DisplayMana(float mf, float mb, float mr, float ml)
     {
