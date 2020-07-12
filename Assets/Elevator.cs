ab131358021cbaf4593df5b669395ccee1bfcf77
Sandsten <staffansandberg@outlook.com>
2020-07-12 08:21:58 +0200

    Elevator door open and closes based on player proximity

---
+++ Assets/Elevator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Open Elevator Door", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Close Elevator Door", true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
