1bbecf433499bc101257d5f789c72d5f248b7750
unknown <elsabenzinger@gmail.com>
2020-07-12 15:14:44 +0200

    working start screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public Sprite oldSprite;
+
+    void OnMouseOver(){
+        spriteRenderer.sprite = newSprite; 
+    }
+
+    void OnMouseExit(){
+        spriteRenderer.sprite = oldSprite;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetMouseButtonDown(0)){
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+         }
+    }
+}
